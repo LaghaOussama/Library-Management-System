c61873f906910e477a2e553368ff251d200e4dc5
Oussama Lagha <lagha.1993@gmail.com>
2026-05-18 16:58:51 +0200

    creating config and security jwt

---
+++ src/main/java/com/Library_Management_System/Configrations/JwtConstant.java
@@ -0,0 +1,8 @@
+package com.Library_Management_System.Configrations;
+
+public class JwtConstant {
+
+    public static final String JWT_HEADER = "Authorization";
+    public static final String SECRET_KEY = "erdhgdresftghdfsgjkdsfgnvsé ";
+
+}
